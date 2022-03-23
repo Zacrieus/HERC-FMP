fe466f4b99e5e862d28080dc9cdabed5c6112593
K4tsy <zachary-mckenzie@hotmail.com>
2022-03-23 18:02:21 +0000

    Boss Shotgun and rotation working

--- HERC UNITY PROJECT/Assets/BossAI.cs
+++ HERC UNITY PROJECT/Assets/BossAI.cs
@@ -14,7 +14,7 @@ public class BossAI : MonoBehaviour
     [Header("Attack Settings")]
     bool canAttack = true;
     float attackTimer;
-    [SerializeField][Range(3,10)]float attackCD;
+    [SerializeField][Range(0,10)]float attackCD;
 
 
     //Setup
@@ -53,26 +53,60 @@ public class BossAI : MonoBehaviour
 
     void random()
     {
-        float rng = Mathf.Round(Random.Range(1f, 1f));
-        Debug.Log(rng);
+        float rng = Mathf.Round(Random.Range(2f, 2f));
+        //Debug.Log(rng);
         
         //attack 1 rannge homing
         if (rng == 1)
         {rangeAttack();}
         else if (rng == 2)
         {
-            Debug.Log("Multishot");
+            shotgunAttack(5,4);
         }
         //attack 2 multishot
     }
 
     void rangeAttack()
     {
-        Debug.Log("Pew");
+        //Debug.Log("Pew");
         hitbox = Instantiate(arrow, transform.position, Quaternion.identity, transform);
-        //hitbox.transform.LookAt(player.transform,);
-        //hitbox.transform.rotation.
-        hitbox.GetComponent<Rigidbody2D>().AddForce(playerDirection * arrowSpeed * 100, ForceMode2D.Force);
+
+        rotateTo2D(hitbox,player.transform.position);
+
+        hitbox.GetComponent<Rigidbody2D>().AddForce(playerDirection * distanceFromPlayer * arrowSpeed * 20, ForceMode2D.Force);
         Object.Destroy(hitbox, 10);
+    }
+
+    void shotgunAttack(float amount,float scale)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            hitbox = Instantiate(arrow, transform.position, Quaternion.identity, transform);
+            float randomX = Random.Range(playerDirection.x - scale/10, playerDirection.x + scale/10);
+            float randomY = Random.Range(playerDirection.y - scale/10, playerDirection.y + scale/10);
+            Vector2 randomDirection = new Vector2(randomX, randomY) *distanceFromPlayer;
+            if (i == 1)
+            {
+                Debug.Log("1....." + randomX + randomY);
+            }
+            else
+            {
+                Debug.Log(randomX+"X" + randomY+"Y");
+            }
+            
+            
+            rotateTo2D(hitbox, randomDirection * arrowSpeed *20);
+            hitbox.GetComponent<Rigidbody2D>().AddForce(randomDirection * arrowSpeed * 20, ForceMode2D.Force);
+        }
+    }
+
+
+    void rotateTo2D(GameObject objecy,Vector3 destination)
+    {
+        //Rotate Towards Player
+        float yVal = objecy.transform.position.y - destination.y;
+        float xVal = objecy.transform.position.x - destination.x;
+        float zAngRotation = Mathf.Atan2(yVal, xVal) * Mathf.Rad2Deg;
+        objecy.transform.rotation = Quaternion.Euler(0, 0, zAngRotation + 90);
     }
 }
